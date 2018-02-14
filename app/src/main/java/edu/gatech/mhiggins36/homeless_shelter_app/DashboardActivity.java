584538274d629164819048ba8d1760d909b369cf
MarcusWilder <marcusnwilder@gmail.com>
2018-02-14 14:43:17 -0500

    Added logout feature

--- app/src/main/java/edu/gatech/mhiggins36/homeless_shelter_app/DashboardActivity.java
+++ app/src/main/java/edu/gatech/mhiggins36/homeless_shelter_app/DashboardActivity.java
@@ -1,7 +1,10 @@
 package edu.gatech.mhiggins36.homeless_shelter_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * App home page
@@ -9,9 +12,17 @@ import android.os.Bundle;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    Button logoutButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        logoutButton = findViewById(R.id.logoutButton);
+    }
+
+    public void logout(View view) {
+        Intent logoutIntent = new Intent(this, MainActivity.class);
+        startActivity(logoutIntent);
     }
 }
