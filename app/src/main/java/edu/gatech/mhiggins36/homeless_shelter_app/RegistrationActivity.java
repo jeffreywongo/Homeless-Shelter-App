25fed817fb843d8b3a7c2ca77b4518dc3bd50fdb
devhiggins <devinhiggins13@gmail.com>
2018-02-20 00:16:50 -0500

    Added a spinner on registration page to select user type.

--- app/src/main/java/edu/gatech/mhiggins36/homeless_shelter_app/RegistrationActivity.java
+++ app/src/main/java/edu/gatech/mhiggins36/homeless_shelter_app/RegistrationActivity.java
@@ -3,7 +3,9 @@ package edu.gatech.mhiggins36.homeless_shelter_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
@@ -12,6 +14,7 @@ public class RegistrationActivity extends AppCompatActivity {
     EditText passField;
     EditText passField2;
     TextView nonMatchingPasswords;
+    Spinner userTypeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,15 @@ public class RegistrationActivity extends AppCompatActivity {
         passField = findViewById(R.id.passwordFieldReg);
         passField2 = findViewById(R.id.passwordFieldReg2);
         nonMatchingPasswords = findViewById(R.id.nonMatchingPasswords);
+
+        //stuff needed for the spinner found online
+        //https://developer.android.com/guide/topics/ui/controls/spinner.html
+        userTypeSpinner = findViewById(R.id.userTypeSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.userTypes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        userTypeSpinner.setAdapter(adapter);
+
     }
 
     public void register(View view) {
