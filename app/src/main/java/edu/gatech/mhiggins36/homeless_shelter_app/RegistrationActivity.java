4640a2664de5070ecf884409fe2434b1a75e2de7
devhiggins <devinhiggins13@gmail.com>
2018-02-20 00:01:04 -0500

    Added onClick method for register button on registration page that checks if passwords match for now. Added a legal name field to registration.

--- app/src/main/java/edu/gatech/mhiggins36/homeless_shelter_app/RegistrationActivity.java
+++ app/src/main/java/edu/gatech/mhiggins36/homeless_shelter_app/RegistrationActivity.java
@@ -2,13 +2,16 @@ package edu.gatech.mhiggins36.homeless_shelter_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     EditText userField;
     EditText passField;
     EditText passField2;
+    TextView nonMatchingPasswords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,13 @@ public class RegistrationActivity extends AppCompatActivity {
         userField = findViewById(R.id.usernameFieldReg);
         passField = findViewById(R.id.passwordFieldReg);
         passField2 = findViewById(R.id.passwordFieldReg2);
+        nonMatchingPasswords = findViewById(R.id.nonMatchingPasswords);
+    }
+
+    public void register(View view) {
+        //if the passwords do not match then reveal red text saying so
+        if (!(passField.equals(passField2))) {
+            nonMatchingPasswords.setVisibility(View.VISIBLE);
+        }
     }
 }
