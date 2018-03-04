ae3d0b8a53fcc47264f60f73d383f2c809888ddb
jeffreywongo <wongojeffrey@yahoo.com>
2018-03-04 15:25:15 -0500

    populated spinners in SearchActivity

--- app/src/main/java/edu/gatech/mhiggins36/homeless_shelter_app/SearchActivity.java
+++ app/src/main/java/edu/gatech/mhiggins36/homeless_shelter_app/SearchActivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchActivity extends AppCompatActivity {
 
-    private TextView nameSearch;
+    private EditText nameSearch;
     private Spinner ageSpinner;
     private Spinner genderSpinner;
 
@@ -22,12 +27,24 @@ public class SearchActivity extends AppCompatActivity {
         nameSearch = findViewById(R.id.nameSearch);
         ageSpinner  = findViewById(R.id.ageSpinner);
         genderSpinner = findViewById(R.id.genderSpinner);
+        // populate the spinners
+        // create adapter for ageSpinner
+        String[] ageSpinnerElems = getResources().getStringArray(R.array.shelterAge);
+        ArrayAdapter<String> ageSpinnerAdapter = new ArrayAdapter<>(this,
+                 android.R.layout.simple_spinner_item, ageSpinnerElems);
+        ageSpinner.setAdapter(ageSpinnerAdapter);
+        // create adapter for genderSpinner
+        String[] genderSpinnerElems = getResources().getStringArray(R.array.shelterGenders);
+        ArrayAdapter<String> genderSpinnerAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, genderSpinnerElems);
+        genderSpinner.setAdapter(genderSpinnerAdapter);
     }
 
     public void search(View view) {
         Intent dashboardIntent = new Intent(this, DashboardActivity.class);
         Intent intent = getIntent();
         String user = intent.getStringExtra("userType");
+        // get the spinner info
 
         dashboardIntent.putExtra("userType", user);
         startActivity(dashboardIntent);
