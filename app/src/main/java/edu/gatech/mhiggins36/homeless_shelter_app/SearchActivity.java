98854813156f241e9063395376190977df670c74
devhiggins <devinhiggins13@gmail.com>
2018-03-04 14:27:29 -0500

    created instances of the fields in SearchActivity

--- app/src/main/java/edu/gatech/mhiggins36/homeless_shelter_app/SearchActivity.java
+++ app/src/main/java/edu/gatech/mhiggins36/homeless_shelter_app/SearchActivity.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private TextView nameSearch;
+    private Spinner ageSpinner;
+    private Spinner genderSpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        nameSearch = findViewById(R.id.nameSearch);
+        ageSpinner  = findViewById(R.id.ageSpinner);
+        genderSpinner = findViewById(R.id.genderSpinner);
     }
 
     public void search(View view) {
